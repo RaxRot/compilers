5aa86f425af463e09b2b52e644e91c7a5b8c9720
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-04-09 15:29:58 +0100

    init

---
+++ src/main/java/com/raxrot/compilers/CompilersApplication.java
@@ -0,0 +1,13 @@
+package com.raxrot.compilers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompilersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompilersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raxrot/compilers/CompilersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.compilers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompilersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
